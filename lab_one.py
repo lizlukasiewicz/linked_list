0b395be09149e97bcedf3356589b040b82015317
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-01-12 11:42:04 -0500

    save work

--- lab_one.py
+++ lab_one.py
@@ -3,7 +3,7 @@ Merge two sorted linked lists and return it as a sorted list.
 The list should be made by splicing together the nodes of the 
 first two lists.
 """
-
+#from Lab import ListNode
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
